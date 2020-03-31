bec400981c6886a09e320d9ca402c5a853d5fb29
Divyansh Singhal <singhaldivyansh1998@gmail.com>
2020-03-30 18:07:44 -0700

    basic login feature and bootstrap

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+function openConnection()
+{
+    $dbhost = "localhost";
+    $dbuser ="root";
+    $dbpass ="root";
+    $db = "zagi";
+    $connection = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $connection -> error);
+    return $connection;
+}
+function closeConnection($connection)
+{
+    $connection -> close();
+}
+?>
